b70b6d9e9fc10932d333eeeb84b8cf467c01aa96
DandelionLau <dandelionlau@163.com>
2018-05-23 09:55:17 +0800

    Create getZCR.m

---
+++ getZCR.m
@@ -0,0 +1,14 @@
+function zcr = getZCR(wavName)
+signal = audioread(wavName);
+framelength = 240;                                                % the length of each frame
+framenumber = fix(length(signal)/framelength);  
+for i = 1:framenumber;                                            % enframe 
+    framesignal = signal((i-1)*framelength+1:i*framelength);      % take one frame signal 
+    zcr(i) = 0;  
+    for j = 2:framelength-1;  
+        if(((sign(framesignal(j))*sign(framesignal(j-1)))<0));    % calcuate the times of zero-crossing 
+       	 zcr(i) = zcr(i) + 1;
+        end
+    end  
+zcr = zcr/framelength;                                            % calcuate the ratio 
+end   
