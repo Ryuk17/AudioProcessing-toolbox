54f3881c71e78fc588581871a479712dd60a8415
DandelionLau <dandelionlau@163.com>
2018-05-29 16:26:40 +0800

    Create getSpecgram.m

---
+++ getSpecgram.m
@@ -0,0 +1,13 @@
+function [X] = getSpecgram(path)
+[signal,f] = audioread(path);
+signal = filter([1 -0.9375],1,signal);            % pre-emphasis
+tframe = enframe(signal,hanning(2048),160);       % enframe in time domain
+[tn,tm]= size(tframe);
+X = zeros(tn,tm/2+1);
+for i = 1:tn
+	Y = fft(tframe(i,:));
+	L = length(tframe(i,:));
+	P2 = abs(Y/L);
+	P1 = P2(1:L/2+1);
+	X(i,:) = P1;
+end
