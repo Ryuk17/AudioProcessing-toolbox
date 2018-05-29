288807e62b626462a019234ae4620055a6cf9a05
DandelionLau <dandelionlau@163.com>
2018-05-29 16:32:55 +0800

    Update getEnergy.m

--- getEnergy.m
+++ getEnergy.m
@@ -1,7 +1,9 @@
 function [E] = getEnergy(path)
+% path: the path of audio sample
+% E: the short time energy of each frame
 [signal,fs] = audioread(path);                                 
 framelength = 240;                                          % set length of window  
-framenumber = fix(length(signal)/framelength);              %    
+framenumber = fix(length(signal)/framelength);                  
 for i = 1:framenumber;                                      % enframe   
     framesignal = signal((i-1)*framelength+1:i*framelength);
     E(i) = 0;                                                 
